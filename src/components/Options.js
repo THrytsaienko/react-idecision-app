ffbd6f40a6ee18b4561479f37f26ed6702ded1cc
T.Hrytsaienko <kortatiana89@gmail.com>
2018-03-08 12:55:46 +0200

    App finished

--- src/components/Options.js
+++ src/components/Options.js
@@ -10,18 +10,18 @@ const Options = (props) => (
 				onClick={props.handleDeleteOptions}>Remove All</button>
 		</div>
 		
-		{props.options.length === 0 && <p className="widget__messagetext">Please add an option to get started!</p>}
-        {
-            props.options.map((option, index) => (
-                <Option
-                    key={option}
+		{props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
+		{
+			props.options.map((option, index) => (
+				<Option
+					key={option}
 					optionText={option}
 					count={index+1}
-                    handleDeleteOption={props.handleDeleteOption}
-                />
-            ))
-        }
-    </div>
+					handleDeleteOption={props.handleDeleteOption}
+				/>
+			))
+		}
+	</div>
 )
 
 export default Options;
